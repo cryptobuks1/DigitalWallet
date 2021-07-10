eb8297e556249f4c34e3d6455e1eb0617e77b23d
alux <ahmadabd13741112@gmail.com>
2021-07-10 19:23:16 +0430

    Make main layout of website by bootstrap: menu by Responsive.

---
+++ resources/views/home.blade.php
@@ -0,0 +1,13 @@
+@extends('layouts.main')
+
+
+@section('title')
+    Digital Wallet
+@endsection
+
+
+@section('content')
+    <h1>Digital Wallet</h1>
+    <p>Digital Wallet is an open source application for Maintaining your expenses.</p>
+    <p>It helps you to organize your future plans.</p>
+@endsection
---
+++ resources/views/layouts/main.blade.php
@@ -0,0 +1,63 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="UTF-8">
+        
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta http-equiv="X-UA-Compatible" content="ie=edge">
+        <meta name="csrf-token" content="{{ csrf_token() }}">
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
+        <link rel="stylesheet" href="css/bootstrap.min.css">
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>
+        <script src="js/bootstrap.min.js"></script>
+
+        <title>@yield("title")</title>
+
+        <!-- Fonts -->
+        <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
+
+        <!-- mycss -->
+        <link rel="stylesheet" href="css/mainLayout.css">
+    </head>
+    <body>
+        <div class="container-fluid">
+            <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+                <div class="container-fluid">
+                    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarTogglerDemo03" aria-controls="navbarTogglerDemo03" aria-expanded="false" aria-label="Toggle navigation">
+                        <span class="navbar-toggler-icon"></span>
+                    </button>
+                    <a class="navbar-brand" href="{{ route('home') }}">Digital Wallet</a>
+                    <div class="collapse navbar-collapse" id="navbarTogglerDemo03">
+                        <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                            @if (Route::has('login'))
+                                @auth
+                                <li class="nav-item">
+                                    <a class="nav-link active" aria-current="page" href="{{ url('/dashboard') }}">Dashboard</a>
+                                </li>
+                                @else
+                                <li class="nav-item">
+                                    <a class="nav-link" href="{{ route('login') }}">Log in</a>
+                                </li>
+                                @if (Route::has('register'))
+                                    <li class="nav-item">
+                                        <a class="nav-link" href="{{ route('register') }}">Register</a>
+                                    </li>
+                                @endif
+                                @endauth
+                            @endif
+                        </ul>
+                    </div>
+                </div>
+            </nav>
+
+            <div class="row contentCss" >
+                <div class="col"></div>
+                <div class="col-6">
+                    @yield('content')
+                </div>
+                <div class="col"></div>
+            </div>
+        </div>
+    </body>
+</html>
