c82a21b7d2d19adac5624094b1c18ce5bc96d933
alux <ahmadabd13741112@gmail.com>
2021-07-26 11:19:50 +0430

    replace @foreach with @forelse

--- resources/views/balenceDetails.blade.php
+++ resources/views/balenceDetails.blade.php
@@ -4,28 +4,31 @@
     Balence Details
 @endsection
 
+@section('css')
+<link rel="stylesheet" href="/css/wallets.css">
+@endsection
 
 @section('content')
-@if(isset($balances))
     <table class="table table-dark" style="border-radius: 8px;">
-    <thead>
-        <tr>
-            <th scope="col">Balance</th>
-            <th scope="col">Total</th>
-            <th scope="col">Date</th>
-        </tr>
-    </thead>
-    <tbody>
-    
-    @foreach($balances as $balance)
-        <tr>
-            <td>{{ $balance['balance'] }}</td>
-            <td>{{ $balance['total'] }}</td>
-            <td>{{ $balance['created_at'] }}</td>
-        </tr>
-    @endforeach
+        <thead>
+            <tr>
+                <th scope="col">Balance</th>
+                <th scope="col">Total</th>
+                <th scope="col">Date</th>
+            </tr>
+        </thead>
+    @forelse($balances as $balance)    
+        <tbody>
+            <tr>
+                <td>{{ $balance['balance'] }}</td>
+                <td>{{ $balance['total'] }}</td>
+                <td>{{ $balance['created_at'] }}</td>
+            </tr>
+        
+        </tbody>
+    @empty
+        <p class="failed">Balence is Empty</p>
+    @endforelse
 
-    </tbody>
     </table>
-@endif
 @endsection
--- resources/views/wallets.blade.php
+++ resources/views/wallets.blade.php
@@ -9,23 +9,21 @@
 @endsection
 
 @section('content')
-    @if(isset($wallets) and !empty($wallets))
-        @foreach($wallets as $wallet)
-            
-                <div class="cards">
-                    <h3>Wallet Name: {{ $wallet['wallet_name'] }}</h3>
-                    <br>
-                    <h5>Total balence: {{ $wallet['total'] }}</h6>
-                    <br>
-                    <h6>created at: {{ $wallet['created_at'] }}</h6>
-                    <br>
-                    <a href="/addBalenceGet/{{ $wallet['id'] }}" class="btn btn-light">Add balence</a>
-                    <a href="/balenceDetails/{{ $wallet['id'] }}" style="margin-left:10px" class="btn btn-success">Details</a>
-                    <a href="/deleteWallet/{{ $wallet['id'] }}" style="margin-left:10px" class="btn btn-danger">Delete</a>
-                </div>
-            
-        @endforeach
-    @else
+
+    @forelse($wallets as $wallet)
+        <div class="cards">
+            <h3>Wallet Name: {{ $wallet['wallet_name'] }}</h3>
+            <br>
+            <h5>Total balence: {{ $wallet['total'] }}</h6>
+            <br>
+            <h6>created at: {{ $wallet['created_at'] }}</h6>
+            <br>
+            <a href="/addBalenceGet/{{ $wallet['id'] }}" class="btn btn-light">Add balence</a>
+            <a href="/balenceDetails/{{ $wallet['id'] }}" style="margin-left:10px" class="btn btn-success">Details</a>
+            <a href="/deleteWallet/{{ $wallet['id'] }}" style="margin-left:10px" class="btn btn-danger">Delete</a>
+        </div>            
+    @empty
         <p class="failed">You have no Wallet yet :(</p>
-    @endif
+    @endforelse
+
 @endsection
